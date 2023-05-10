a6963971adaba3d80740c225dc5bd189d7ff682b
itamar hess <Itamardarihess@gmail.com>
2023-05-10 23:31:00 +0300

    connection bugs

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const io = require("socket.io")(3000, {
     }
 });
 
-io.on("connection", ws => {
+
+io.on("connection", socket => {
+    let prevRoom;
+
+    socket.on("join game", room => {
+        //console.log("leave");
+        console.log(room);
+        socket.join(room);
+        let numberOfUsers = io.sockets.adapter.rooms.get(room).size;
+        //console.log(numberOfUsers);
+    });
 
 });
